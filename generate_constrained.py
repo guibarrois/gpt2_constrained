aedcadb1b7a4fcb852b93b91e2cbfe63602402ac
guillaumebarrois <gbarrois@liegeymullerpons.com>
2026-08-17 22:26:50 +0200

    add cache on generation

--- generate_constrained.py
+++ generate_constrained.py
@@ -11,19 +11,26 @@ model.eval()
 
 auth_ids = torch.as_tensor(auth_ids)
 
+torch.set_num_threads(2)
 logger = logging.getLogger(__name__)
 
 def generate_constrained(input_sentence, max_new_tokens=10, temperature=0.5, best=True):
+    past = None
     enc = tok(input_sentence, return_tensors="pt")
     ids = enc.input_ids
+    nb_new_tokens = 0
     for _ in range(max_new_tokens):
-        out = model(
-            input_ids=ids, 
-            attention_mask=enc.attention_mask, 
-            max_new_tokens=1, 
-            pad_token_id=tok.eos_token_id, 
-            do_sample=False,
-        )
+        with torch.no_grad():
+            out = model(
+                input_ids=ids, 
+                attention_mask=enc.attention_mask, 
+                max_new_tokens=1, 
+                pad_token_id=tok.eos_token_id, 
+                do_sample=False,
+                use_cache=True,
+                past_key_values=past
+            )
+        past = out.past_key_values
 
         logits = out.logits[:, -1, :]
         auth_logits = logits[:, auth_ids]
@@ -36,6 +43,7 @@ def generate_constrained(input_sentence, max_new_tokens=10, temperature=0.5, bes
             next_token_id = auth_ids[sampled_best]
         ids = torch.cat([ids, next_token_id], dim=-1)
         logger.info(f"Generated token: {tok.decode(next_token_id[0])}")
+        nb_new_tokens += 1
         if next_token_id.item() == tok.eos_token_id:
             break
-    return tok.decode(ids[0])
+    return tok.decode(ids[0]), nb_new_tokens
