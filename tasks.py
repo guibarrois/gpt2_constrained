426510e2f7d74705aefb5eec7d66a145fcec1ed2
guillaumebarrois <gbarrois@liegeymullerpons.com>
2026-08-18 22:39:30 +0200

    add model loading as an initial server step

--- tasks.py
+++ tasks.py
@@ -1,14 +1,24 @@
 from celery import Celery
-from generate_constrained import generate_constrained
+import logging
+from generate_constrained import generate_constrained, load_model
 from celery.signals import worker_process_init
 
 _model = None
+
+logger = logging.getLogger(__name__)
 
 app = Celery(
     'tasks',
     broker='redis://localhost:6379/0',
     backend='redis://localhost:6379/1')
 
+@worker_process_init.connect
+def init_worker(**kwargs):
+    logger.info("Initializing worker process...")
+    load_model()
+    logger.info("Model loaded successfully.")
+
+    
 @app.task(bind=True)
 def generate(self, input_sentence, max_new_tokens=10, temperature=0.5, best=True):
     completed_text, nb_new_tokens = generate_constrained(
